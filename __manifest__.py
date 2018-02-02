15160db0fee5ac8b7d653f8b81b32b51a5f9afe9
Duy-Nguyen-Van <vanduy1309@gmail.com>
2018-02-02 14:23:13 +0700

    Create-Module

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+	'name': 'Document Management',
+	'description': 'Manage your personal Document tasks.',
+	'author': 'Duy Nguyen',
+	'depends': ['base','mail'],
+	'application': True,
+	# 'images':['images/icon.png'],
+	'data': [
+		'views/qlcv_menu.xml',
+        'views/qlcv_view.xml'
+	],
+}
---
+++ qlcv_model.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields
+
+class Document(models.Model):
+     _name = 'doc.task'
+     _description = 'Document'
+     _inherit = ['mail.thread']
+     name = fields.Char('Name',required=True)
+     number = fields.Char('Number',required=True)
+     sign_date = fields.Datetime('Sign Date')
+     sent_date = fields.Datetime('Sent Date')
+     arrived_date = fields.Datetime('Arrived Date')
+     composer_id = fields.Many2one('hr.employee' , 'Composer')
+     approver_id = fields.Many2one('hr.employee' , 'Approver')
+     sender_id = fields.Many2one('hr.employee' , 'Sender')
+     customer = fields.Many2one('res.partner','Customer',required=True)
+     department_id = fields.Many2one('hr.department' , 'Department')
+     # type = fields.Char('Type',required=True)
+     # notes = fields.Text('Note')
+     type_id = fields.Many2one('doc.type','Type',required=True)
+     file = fields.Binary('File',required=True)
+     state = fields.Selection([
+          ('draft','Draft'),
+          ('published','Published'),
+          ('sent','Sent'),
+          ('done','Done'),
+     ],string='Document Status', readonly=True, copy=False, store=True, default='draft')
+
+# class Department(models.Model):
+#      _name = 'department.task'
+#      _description = 'Department'
+#      _inherit = ['mail.thread']
+#      iden = fields.Char('ID',required=True)
+#      name = fields.Char('Name',required=True)
+
+class Document_Type(models.Model):
+     _name = 'doc.type'
+     _description = 'Document Type'
+     name = fields.Char('Name',required=True)
+     des = fields.Char('Description')
+
+class Document_Sent(models.Model):
+     _inherit = 'doc.task'
+     _name = 'doc.sent'
+
+@api.multi
+def import_file(self, cr, uid, ids, context=None):
+    fileobj = TemporaryFile('w+')
+    fileobj.write(base64.decodestring(data))
+    # your treatment
+    return True
+
+
+
+
+
+
